45c3a7ecf08a8cf222ddab3a12bf167f0ec6b55a
Dreamwalker <aristojeff@gmail.com>
2019-02-10 13:06:04 +0900

    Update browse_page.dart

--- lib/reading_book_part1/page/page/browse_page.dart
+++ lib/reading_book_part1/page/page/browse_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class BrowsePage extends StatelessWidget {
@@ -30,12 +31,10 @@ class _DetailHomePageState extends State<DetailHomePage> {
               fontWeight: FontWeight.bold, color: Colors.black, fontSize: 30.0),
         ),
       ),
-
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-            
             height: MediaQuery.of(context).size.height - 180.0,
             width: MediaQuery.of(context).size.width - 32.0,
             decoration: BoxDecoration(
@@ -44,17 +43,144 @@ class _DetailHomePageState extends State<DetailHomePage> {
             ),
             child: ListView(
               children: <Widget>[
-
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 16.0),
                   child: Container(
                     height: 160.0,
                     width: double.infinity,
-                    color: Colors.yellow,
+//                    color: Colors.yellow,
                     child: Stack(
                       children: <Widget>[
-                        
+                        Positioned(
+                          left: 60.0,
+                          child: Container(
+                            height: 160,
+                            width: 280,
+//                            color: Colors.orange,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(7.0),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black45.withOpacity(0.2),
+                                    offset: Offset(0.0, 2.0),
+                                    blurRadius: 1.0,
+                                    spreadRadius: 2.0,
+                                  )
+                                ]),
 
+                            child: Row(children: <Widget>[
+                              SizedBox(
+                                width: 70,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(),
+                                      child: Text(
+                                        "Earthsea",
+                                        style: TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 8.0,
+                                    ),
+                                    Row(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 20.0,
+                                        ),
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 20.0,
+                                        ),
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 20.0,
+                                        ),
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.yellow,
+                                          size: 20.0,
+                                        ),
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.grey,
+                                          size: 20.0,
+                                        ),
+                                        SizedBox(
+                                          width: 8.0,
+                                        ),
+                                        Text(
+                                          "8.6",
+                                          style: TextStyle(color: Colors.grey),
+                                        )
+                                      ],
+                                    ),
+
+                                    SizedBox(
+                                      height: 24.0,
+                                    ),
+                                    Container(
+                                      height: 60.0,
+                                      width: 200.0,
+
+                                      child: AutoSizeText(""
+                                          "asjdlsajdlksajdlsjkldsajdsdasdjsasajdlsajdlsjldajdasjj",
+                                      style: TextStyle(color: Colors.grey),),
+                                    )
+//                                    Text("To the world you may one ", style: TextStyle(
+//                                      color: Colors.black45
+//                                    ),),
+
+                                  ],
+                                ),
+
+                              ),
+                            ]),
+                          ),
+                        ),
+                        Positioned(
+                          top: 8.0,
+                          child: Container(
+                            height: 140.0,
+                            width: 110.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black45.withOpacity(0.2),
+                                    offset: Offset(0.0, 2.0),
+                                    blurRadius: 1.0,
+                                    spreadRadius: 2.0,
+                                  )
+                                ]),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(7.0),
+                                child: Image.network(
+                                  "https://cdn.pixabay.com/photo/2017/03/05/21/06/bird-2119874_960_720.jpg",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -64,7 +190,6 @@ class _DetailHomePageState extends State<DetailHomePage> {
           ),
         ),
       ),
-
       bottomNavigationBar: Theme(
         data: Theme.of(context).copyWith(canvasColor: Colors.white),
         child: BottomNavigationBar(
@@ -85,11 +210,3 @@ class _DetailHomePageState extends State<DetailHomePage> {
     );
   }
 }
-
-
-
-
-
-
-
-
