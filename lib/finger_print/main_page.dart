167f02bf0a34dd3d277ecc82a332898851e7a8f2
Dreamwalker <aristojeff@gmail.com>
2019-02-14 00:33:09 +0900

    aa

---
+++ lib/finger_print/main_page.dart
@@ -0,0 +1,51 @@
+
+
+
+
+
+import 'package:flutter/material.dart';
+
+
+
+class FingerMainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
