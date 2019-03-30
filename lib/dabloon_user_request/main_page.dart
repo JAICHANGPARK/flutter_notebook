b22008d62d72e9fc3f01208685040030449daa71
Dreamwalker <aristojeff@gmail.com>
2019-03-31 00:11:54 +0900

    Update main_page.dart

--- lib/dabloon_user_request/main_page.dart
+++ lib/dabloon_user_request/main_page.dart
@@ -1,7 +1,4 @@
-
-
 import 'package:flutter/material.dart';
-
 
 class DabloonUserRequestApp extends StatelessWidget {
   @override
@@ -11,12 +8,24 @@ class DabloonUserRequestApp extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+  TabController _tabController;
+  TabController _tabDetailController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabDetailController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,38 +36,143 @@ class _MainPageState extends State<MainPage> {
             top: 0,
             left: 0,
             right: 0,
-            child:Container(
+            child: Container(
               height: 240,
               width: MediaQuery.of(context).size.width,
-              color: Colors.redAccent,
+              color: Colors.pinkAccent,
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.arrow_back, color: Colors.white,
-                    size: 32,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 28),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                      onPressed: () {},
                     ),
-                    onPressed: (){},
                   ),
-                  SizedBox(height: 16.0,),
-                  ListView(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    children: <Widget>[
-                      Text("Marble Statues"),
-                      Text("Coins")
-                    ],
-                  )
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  _buildTopTabBar(context),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: _buildSecondaryTabBar(context),
+                  ),
                 ],
               ),
             ),
           )
-
         ],
       ),
     );
   }
-}
 
+  Widget _buildTopTabBar(BuildContext context) {
+    return TabBar(
+      onTap: (int value) {
+        print(value);
+      },
+      controller: _tabController,
+      indicatorColor: Colors.transparent,
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.grey.withOpacity(0.7),
+      isScrollable: true,
+      tabs: <Widget>[
+        Tab(
+          child: Text(
+            "Marble Statues",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 38),
+          ),
+        ),
+        Tab(
+          child: Text(
+            "Coins",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 38),
+          ),
+        ),
+        Tab(
+          child: Text(
+            "artificial",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 38),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildSecondaryTabBar(BuildContext context) {
+    return TabBar(
+      onTap: (int value) {
+        print(value);
+      },
+      controller: _tabDetailController,
+      indicatorColor: Colors.transparent,
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.grey.withOpacity(0.7),
+      isScrollable: true,
+      tabs: <Widget>[
+        Tab(
+          child: Text(
+            "All",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 18),
+          ),
+        ),
+        Tab(
+          child: Text(
+            "Rare",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 18),
+          ),
+        ),
+        Tab(
+          child: Text(
+            "Greek",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 18),
+          ),
+        ),
+        Tab(
+          child: Text(
+            "Roman",
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 18),
+          ),
+        )
+      ],
+    );
+  }
+}
