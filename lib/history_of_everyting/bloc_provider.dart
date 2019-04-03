468c5da02ac7c9b5002408ba25adeb30ea997ce9
Dreamwalker <aristojeff@gmail.com>
2019-04-03 22:01:23 +0900

    Update !

--- lib/history_of_everyting/bloc_provider.dart
+++ lib/history_of_everyting/bloc_provider.dart
@@ -1,54 +1,64 @@
-//import 'package:flutter/material.dart';
-//import 'package:flutter/widgets.dart';
-//import 'package:flutter_notebook/history_of_everyting/blocs/favorites_bloc.dart';
-//import 'package:flutter_notebook/history_of_everyting/timeline/timeline.dart';
-//
-//class BlocProvider extends InheritedWidget {
-//  final FavoritesBloc favoritesBloc;
-//  final Timeline timeline;
-//
-//  BlocProvider(
-//      {Key key,
-//      FavoritesBloc fb,
-//      Timeline t,
-//      @required Widget child,
-//      TargetPlatform platform = TargetPlatform.iOS})
-//      : timeline = t ?? Timeline(platform),
-//        favoritesBloc = fb ?? FavoritesBloc(),
-//        super(key: key, child: child){
-//
-//
-//    //TODo Create Timeline dart file and set init
-//    // ToDo Timeline Setting
-//
-//    //TODO CREATE SEARCH MANAGER
-//
-//  };
-//
-//  @override
-//  bool updateShouldNotify(InheritedWidget oldWidget) {
-//    // TODO: implement updateShouldNotify
-//    return true;
-//  }
-//
-//  static FavoritesBloc favorites(BuildContext context){
-//    BlocProvider bp = (context.inheritFromWidgetOfExactType(BlocProvider) as BlocProvider);
-//    FavoritesBloc bloc = bp?.favoritesBloc;
-//    return bloc;
-//  }
-//
-//  //TODO MADE getTimeLine Method
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+import "package:flutter/widgets.dart";
+import 'package:flutter_notebook/history_of_everyting/blocs/favorites_bloc.dart';
+import 'package:flutter_notebook/history_of_everyting/search_manager.dart';
+import 'package:flutter_notebook/history_of_everyting/timeline/timeline.dart';
+import 'package:flutter_notebook/history_of_everyting/timeline/timeline_entry.dart';
+
+/// This [InheritedWidget] wraps the whole app, and provides access
+/// to the user's favorites through the [FavoritesBloc] 
+/// and the [Timeline] object.
+class BlocProvider extends InheritedWidget {
+  final FavoritesBloc favoritesBloc;
+  final Timeline timeline;
+
+  /// This widget is initialized when the app boots up, and thus loads the resources.
+  /// The timeline.json file contains all the entries' data.
+  /// Once those entries have been loaded, load also all the favorites.
+  /// Lastly use the entries' references to load a local dictionary for the [SearchManager].
+  BlocProvider(
+      {Key key,
+        FavoritesBloc fb,
+        Timeline t,
+        @required Widget child,
+        TargetPlatform platform = TargetPlatform.iOS})
+      : timeline = t ?? Timeline(platform),
+        favoritesBloc = fb ?? FavoritesBloc(),
+        super(key: key, child: child) {
+    timeline
+        .loadFromBundle("assets/timeline.json")
+        .then((List<TimelineEntry> entries) {
+      timeline.setViewport(
+          start: entries.first.start * 2.0,
+          end: entries.first.start,
+          animate: true);
+      /// Advance the timeline to its starting position.
+      timeline.advance(0.0, false);
+
+      /// All the entries are loaded, we can fill in the [favoritesBloc]...
+      favoritesBloc.init(entries);
+      /// ...and initialize the [SearchManager].
+      SearchManager.init(entries);
+    });
+  }
+
+  @override
+  updateShouldNotify(InheritedWidget oldWidget) => true;
+
+  /// static accessor for the [FavoritesBloc]. 
+  /// e.g. [ArticleWidget] retrieves the favorites information using this static getter.
+  static FavoritesBloc favorites(BuildContext context) {
+    BlocProvider bp =
+    (context.inheritFromWidgetOfExactType(BlocProvider) as BlocProvider);
+    FavoritesBloc bloc = bp?.favoritesBloc;
+    return bloc;
+  }
+
+  /// static accessor for the [Timeline]. 
+  /// e.g. [_MainMenuWidgetState.navigateToTimeline] uses this static getter to access build the [TimelineWidget].
+  static Timeline getTimeline(BuildContext context) {
+    BlocProvider bp =
+    (context.inheritFromWidgetOfExactType(BlocProvider) as BlocProvider);
+    Timeline bloc = bp?.timeline;
+    return bloc;
+  }
+}
--- lib/history_of_everyting/timeline/timeline_entry.dart
+++ lib/history_of_everyting/timeline/timeline_entry.dart
@@ -11,6 +11,11 @@ import 'package:flutter_notebook/nima/nima/animation/actor_animation.dart'
     as nima;
 import 'package:flutter_notebook/nima/nima/math/aabb.dart' as nima;
 
+
+/// An object representing the renderable assets loaded from `timeline.json`.
+///
+/// Each [TimelineAsset] encapsulates all the relevant properties for drawing,
+/// as well as maintaining a reference to its original [TimelineEntry].
 class TimelineAsset {
   double width;
   double height;
@@ -23,10 +28,12 @@ class TimelineAsset {
   TimelineEntry entry;
 }
 
+/// A renderable image.
 class TimelineImage extends TimelineAsset {
   ui.Image image;
 }
 
+/// This asset also has information regarding its animations.
 class TimelineAnimatedAsset extends TimelineAsset {
   bool loop;
   double animationTime = 0.0;
@@ -34,6 +41,7 @@ class TimelineAnimatedAsset extends TimelineAsset {
   double gap = 0.0;
 }
 
+/// An `Nima` Asset.
 class TimelineNima extends TimelineAnimatedAsset {
   nima.FlutterActor actorStatic;
   nima.FlutterActor actor;
@@ -41,33 +49,55 @@ class TimelineNima extends TimelineAnimatedAsset {
   nima.AABB setupAABB;
 }
 
+/// A `Flare` Asset.
 class TimelineFlare extends TimelineAnimatedAsset {
   flare.FlutterActorArtboard actorStatic;
   flare.FlutterActorArtboard actor;
   flare.ActorAnimation animation;
 
+  /// Some Flare assets will have multiple idle animations (e.g. 'Humans'),
+  /// others will have an intro&idle animation (e.g. 'Sun is Born').
+  /// All this information is in `timeline.json` file, and it's de-serialized in the
+  /// [Timeline.loadFromBundle()] method, called during startup.
+  /// and custom-computed AABB bounds to properly position them in the timeline.
   flare.ActorAnimation intro;
   flare.ActorAnimation idle;
-  List<flare.ActorAnimation> idleAnimation;
+  List<flare.ActorAnimation> idleAnimations;
   flare.AABB setupAABB;
+
 }
 
+/// A label for [TimelineEntry].
 enum TimelineEntryType { Era, Incident }
 
+/// Each entry in the timeline is represented by an instance of this object.
+/// Each favorite, search result and detail page will grab the information from a reference
+/// to this object.
+///
+/// They are all initialized at startup time by the [BlocProvider] constructor.
 class TimelineEntry {
-  TimelineEntry type;
+  TimelineEntryType type;
+
+  /// Used to calculate how many lines to draw for the bubble in the timeline.
   int lineCount = 1;
+  ///
   String _label;
   String articleFilename;
   String id;
+
   Color accent;
 
-  TimelineEntry parant;
+  /// Each entry constitues an element of a tree:
+  /// eras are grouped into spanning eras and events are placed into the eras they belong to.
+  TimelineEntry parent;
   List<TimelineEntry> children;
-
+  /// All the timeline entries are also linked together to easily access the next/previous event.
+  /// After a couple of seconds of inactivity on the timeline, a previous/next entry button will appear
+  /// to allow the user to navigate faster between adjacent events.
   TimelineEntry next;
   TimelineEntry previous;
 
+  /// All these parameters are used by the [Timeline] object to properly position the current entry.
   double start;
   double end;
   double y = 0.0;
@@ -91,10 +121,9 @@ class TimelineEntry {
     return opacity > 0.0;
   }
 
-  ///this is getter setter in dart
-  ///getter setter are using java --Dreamwalker
   String get label => _label;
-
+  /// Some labels already have newline characters to adjust their alignment.
+  /// Detect the occurrence and add information regarding the line-count.
   set label(String value) {
     _label = value;
     int start = 0;
@@ -109,6 +138,7 @@ class TimelineEntry {
     }
   }
 
+  /// Pretty-printing for the entry date.
   String formatYearsAgo() {
     if (start > 0) {
       return start.round().toString();
@@ -116,26 +146,29 @@ class TimelineEntry {
     return TimelineEntry.formatYears(start) + " Ago";
   }
 
+  /// Debug information.
   @override
   String toString() {
-    return "TIMELINE ENTRY : $label - ($start,$end)";
+    return "TIMELINE ENTRY: $label -($start,$end)";
   }
 
+  /// Helper method.
   static String formatYears(double start) {
     String label;
-    int valueAbs = start.round().abs(); // |start.round|
+    int valueAbs = start.round().abs();
     if (valueAbs > 1000000000) {
-      // 1g
       double v = (valueAbs / 100000000.0).floorToDouble() / 10.0;
+
       label = (valueAbs / 1000000000)
-              .toStringAsFixed(v == v.floorToDouble() ? 0 : 1) +
+          .toStringAsFixed(v == v.floorToDouble() ? 0 : 1) +
           " Billion";
     } else if (valueAbs > 1000000) {
       double v = (valueAbs / 100000.0).floorToDouble() / 10.0;
       label =
           (valueAbs / 1000000).toStringAsFixed(v == v.floorToDouble() ? 0 : 1) +
               " Million";
-    } else if (valueAbs > 10000) {
+    } else if (valueAbs > 10000) // N.B. < 10,000
+        {
       double v = (valueAbs / 100.0).floorToDouble() / 10.0;
       label =
           (valueAbs / 1000).toStringAsFixed(v == v.floorToDouble() ? 0 : 1) +
