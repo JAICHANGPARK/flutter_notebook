8e3e5dbadb133caca9969a2f7c24dc4bb3b2aa40
Dreamwalker <aristojeff@gmail.com>
2019-04-10 22:07:14 +0900

    Update image_drawing.dart

--- lib/basic_for_basic/paint_ui/image_drawing.dart
+++ lib/basic_for_basic/paint_ui/image_drawing.dart
@@ -6,14 +6,12 @@ import 'package:flutter/rendering.dart';
 import 'dart:typed_data';
 import 'dart:async';
 
-
 class ImageDrawingPage extends StatefulWidget {
   @override
   _ImageDrawingPageState createState() => _ImageDrawingPageState();
 }
 
 class _ImageDrawingPageState extends State<ImageDrawingPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,22 +19,23 @@ class _ImageDrawingPageState extends State<ImageDrawingPage> {
         title: Text("Image"),
         centerTitle: true,
       ),
+      body: Center(
+        child: CustomRenderBoxWidget(),
+      ),
     );
   }
 }
 
-
-class CustomRenderBoxWidget extends SingleChildRenderObjectWidget{
+class CustomRenderBoxWidget extends SingleChildRenderObjectWidget {
   @override
   RenderObject createRenderObject(BuildContext context) {
     // TODO: implement createRenderObject
     return _RenderBox();
   }
 }
-class _RenderBox extends RenderBox{
 
+class _RenderBox extends RenderBox {
   ui.Image _image;
-
 
   @override
   bool hitTest(HitTestResult result, {Offset position}) {
@@ -44,15 +43,16 @@ class _RenderBox extends RenderBox{
     return true;
   }
 
-  _RenderBox(){
+  _RenderBox() {
     loadAssetImage('picsix.jpeg');
   }
 
+  //TODO : load -> uint8List -> instantiateImageCodec -> getNextFrame
   loadAssetImage(String fileName) =>
-      rootBundle.load("assets/img/$fileName").then((bd){
+      rootBundle.load("assets/img/$fileName").then((bd) {
         Uint8List uint8list = Uint8List.view(bd.buffer);
-        ui.instantiateImageCodec(uint8list).then((codec){
-          codec.getNextFrame().then((frameInfo){
+        ui.instantiateImageCodec(uint8list).then((codec) {
+          codec.getNextFrame().then((frameInfo) {
             _image = frameInfo.image;
             markNeedsPaint();
           });
@@ -68,20 +68,16 @@ class _RenderBox extends RenderBox{
     int dy = offset.dy.toInt();
 
     Paint paint = Paint();
-    Offset off = Offset(dx + 50.0, dy + 50.0);
-    if(_image != null){
-      canvas.drawImage(_image, off, paint);
+    Offset off = Offset(dx.toDouble(), dy.toDouble());
+    Rect rect = Rect.fromLTWH(dx + 50.0, dy + 50.0, 300, 200);
+    if (_image != null) {
+      Rect rect0 = Rect.fromLTWH(
+          0, 0, _image.width.toDouble(), _image.height.toDouble());
+//      canvas.drawImage(_image, off, paint);\
+      canvas.drawImageRect(_image, rect0, rect, paint);
     }
   }
 }
-
-
-
-
-
-
-
-
 
 
 
