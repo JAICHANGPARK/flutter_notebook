992bdcfecc0c30353dce7918697f307c39d2465e
Dreamwalker <aristojeff@gmail.com>
2019-04-14 16:52:10 +0900

    Update main_page.dart

---
+++ lib/elements_app/main_page.dart
@@ -0,0 +1,47 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class ElementsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Element"),
+      ),
+      body: Container(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
