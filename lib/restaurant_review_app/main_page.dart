af77b631f5dfaae5bb3e3ffe81cbf7141c2a023c
Dreamwalker <aristojeff@gmail.com>
2019-04-21 20:58:52 +0900

    Update main_page.dart

--- lib/restaurant_review_app/main_page.dart
+++ lib/restaurant_review_app/main_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class RestaurantReviewCloneApp extends StatelessWidget {
@@ -15,33 +14,78 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> 
-with SingleTickerProviderStateMixin{
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile",),
+        title: Text(
+          "Profile",
+          style: TextStyle(
+            color: Colors.grey,
+          ),
+        ),
+        leading: Icon(
+          Icons.arrow_back_ios,
+          color: Colors.grey,
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: Icon(
+              Icons.edit,
+              color: Colors.grey,
+            ),
+          )
+        ],
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
-      body: Container(),
+      body: ListView(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 15.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      image: DecorationImage(image: 
+                      NetworkImage("https://avatars2.githubusercontent.com/u/19484515?s=460&v=4",
+                      ),
+                      fit: BoxFit.cover)
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 210.0,
+                top: 70.0,
+                child:Container(
+                  
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
-
-
-
-
-
-
-
 
 
 
