8b79ec96d1c50c6b8fb956df496612708282ed17
Dreamwalker <aristojeff@gmail.com>
2019-04-22 20:51:51 +0900

    Update

---
+++ lib/story_sample_app/data.dart
@@ -0,0 +1,15 @@
+
+
+List<String> images =[
+  "https://cdn.pixabay.com/photo/2019/04/20/11/39/japan-4141578_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2014/04/14/20/11/japanese-cherry-trees-324175_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014618_960_720.jpg",
+  "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014616_960_720.jpg"
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Castel",
+];
--- lib/story_sample_app/main_page.dart
+++ lib/story_sample_app/main_page.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook/story_sample_app/data.dart';
 
 class StoryCloneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
       home: HomePage(),
     );
   }
 }
 
-var cardAspectRatio = 12.0/ 16.0;
+var cardAspectRatio = 12.0 / 16.0;
 var widgetAspectRatio = cardAspectRatio * 1.2;
 
 class HomePage extends StatefulWidget {
@@ -20,26 +19,191 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var currentPage = images.length - 1.0;
 
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page;
+      });
+    });
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient( colors: [
-          Color(0xFF1b1e44),
-          Color(0xFF2d3447),
-        ],
-        begin: Alignment.bottomCenter,
-          end: Alignment.topCenter,
-          tileMode: TileMode.clamp
-        )
+          gradient: LinearGradient(
+              colors: [
+            Color(0xFF1b1e44),
+            Color(0xFF2d3447),
+          ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              tileMode: TileMode.clamp)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Trending",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          letterSpacing: 1.0),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.settings,
+                        size: 12.0,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color(0xFFff6e6e),
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 22.0, vertical: 6.0),
+                          child: Text(
+                            "Animated",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      "25+ Stories",
+                      style: TextStyle(color: Colors.blueAccent),
+                    )
+                  ],
+                ),
+              ),
+              Stack(
+                children: <Widget>[
+                  Positioned.fill(
+                    child: PageView.builder(
+                      itemCount: images.length,
+                      controller: controller,
+                      itemBuilder: (context, index) {
+                        return Container();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Favourtie",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          letterSpacing: 1.0),
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.white,
+                        size: 12.0,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 22.0, vertical: 6.0),
+                          child: Text(
+                            "Latest",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 15.0,),
+                    Text("9+ Stories",
+                    style: TextStyle(
+                      color: Colors.blueAccent
+                    ),)
+                  ],
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 18.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.network(src,
+                      width: 296.0, height: 222.0,),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
-
-
 
 
 
