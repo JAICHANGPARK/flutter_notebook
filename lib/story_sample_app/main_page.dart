8f72548e18aa89a74add20b432d26ca5d023811f
Dreamwalker <aristojeff@gmail.com>
2019-04-22 21:12:23 +0900

    Update main_page.dart

--- lib/story_sample_app/main_page.dart
+++ lib/story_sample_app/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook/story_sample_app/data.dart';
 
@@ -33,9 +35,9 @@ class _HomePageState extends State<HomePage> {
       decoration: BoxDecoration(
           gradient: LinearGradient(
               colors: [
-            Color(0xFF1b1e44),
-            Color(0xFF2d3447),
-          ],
+                Color(0xFF1b1e44),
+                Color(0xFF2d3447),
+              ],
               begin: Alignment.bottomCenter,
               end: Alignment.topCenter,
               tileMode: TileMode.clamp)),
@@ -123,6 +125,7 @@ class _HomePageState extends State<HomePage> {
               ),
               Stack(
                 children: <Widget>[
+                  CardScrollWidget(currentPage),
                   Positioned.fill(
                     child: PageView.builder(
                       itemCount: images.length,
@@ -134,6 +137,8 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ],
               ),
+
+
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20.0),
                 child: Row(
@@ -176,23 +181,30 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                    SizedBox(width: 15.0,),
-                    Text("9+ Stories",
-                    style: TextStyle(
-                      color: Colors.blueAccent
-                    ),)
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      "9+ Stories",
+                      style: TextStyle(color: Colors.blueAccent),
+                    )
                   ],
                 ),
               ),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               Row(
                 children: <Widget>[
                   Padding(
                     padding: EdgeInsets.only(left: 18.0),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20.0),
-                      child: Image.network(src,
-                      width: 296.0, height: 222.0,),
+                      child: Image.network(
+                        "https://github.com/devefy/Flutter-Story-App-UI/blob/master/assets/image_02.jpg?raw=true",
+                        width: 296.0,
+                        height: 222.0,
+                      ),
                     ),
                   )
                 ],
@@ -204,6 +216,122 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
+
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(builder: (context, contraints) {
+        var width = contraints.maxWidth;
+        var height = contraints.maxHeight;
+
+        var safeWidth = width - 2 * padding;
+        var safeHeight = height - 2 * padding;
+
+        var heightOfPrimaryCard = safeHeight;
+        var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        var horizontalInset = primaryCardLeft / 2;
+        List<Widget> cardList = new List();
+
+        for (var i = 0; i < images.length; i++) {
+          var delta = i - currentPage;
+          bool isOnRight = delta > 0;
+          var start = padding +
+              max(
+                  primaryCardLeft -
+                      horizontalInset * -delta * (isOnRight ? 15 : 1),
+                  0.0);
+
+          var cardItem = Positioned.directional(
+              top: padding + verticalInset * max(-delta, 0.0),
+              bottom: padding + verticalInset * max(-delta, 0.0),
+              start: start,
+              textDirection: TextDirection.rtl,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(3.0, 6.0),
+                        blurRadius: 10.0
+                      )
+                    ]
+                  ),
+                  child: AspectRatio(aspectRatio: cardAspectRatio,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      Image.network(images[i],fit: BoxFit.cover,),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 16.0,
+                                vertical: 8.0,
+                              ),
+                              child: Text(title[i],
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25.0
+                              ),),
+                            ),
+                            SizedBox(height: 10.0,),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 12.0, bottom: 12.0
+                              ),
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 22.0,
+                                  vertical: 6.0
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius: BorderRadius.circular(20.0)
+                                ),
+                                child: Text("Read Later",
+                                style: TextStyle(
+                                  color: Colors.white
+                                ),),
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),),
+                ),
+              ));
+          cardList.add(cardItem);
+        }
+        return Stack(
+          children: cardList,
+        );
+      }),
+    );
+  }
+}
+
+
+
+
+
 
 
 
