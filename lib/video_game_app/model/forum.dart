bd5b0af786cb653c40c392c8a89d7b7f20cbd608
Dreamwalker <aristojeff@gmail.com>
2019-04-26 22:13:53 +0900

    Update main code

--- lib/video_game_app/model/forum.dart
+++ lib/video_game_app/model/forum.dart
@@ -1,8 +1,6 @@
-
-
 import 'package:flutter_notebook/video_game_app/model/topic.dart';
 
-class Forum{
+class Forum {
   final String title;
   final String imagePath;
   final String rank;
@@ -10,8 +8,46 @@ class Forum{
   final String threads;
   final String subs;
 
-  Forum(this.title, this.imagePath, this.rank, this.topics, this.threads,
-      this.subs);
-
-
+  Forum(
+      {this.title,
+      this.imagePath,
+      this.rank,
+      this.topics,
+      this.threads,
+      this.subs});
 }
+
+final fortniteForum = Forum(
+    title: "Fortnite",
+    imagePath: "https://github.com/TechieBlossom/video_game_messaging_app/blob/part2/assets/images/fortnite.jpg?raw=true",
+    rank: "31",
+    threads: "93",
+    subs: "1k+",
+    topics: fortniteTopics);
+
+final pubgForum = Forum(
+    title: "PUBG",
+    imagePath: "https://github.com/TechieBlossom/video_game_messaging_app/blob/part2/assets/images/pubg.png?raw=true",
+    rank: "25",
+    threads: "120",
+    subs: "1000+",
+    topics: pubgTopics);
+
+
+final forums = [fortniteForum, pubgForum];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/video_game_app/ui/horizontal_tab_layour.dart
+++ lib/video_game_app/ui/horizontal_tab_layour.dart
@@ -6,9 +6,28 @@ class HorizontalTabLayout extends StatefulWidget {
   _HorizontalTabLayoutState createState() => _HorizontalTabLayoutState();
 }
 
-class _HorizontalTabLayoutState extends State<HorizontalTabLayout> {
-
+class _HorizontalTabLayoutState extends State<HorizontalTabLayout>
+    with SingleTickerProviderStateMixin {
   int selectedTabIndex = 2;
+  AnimationController _controller;
+  Animation<Offset> _animation;
+  Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _animation = Tween<Offset>(begin: Offset(0.0, 0.0), end: Offset(-0.05, 0))
+        .animate(_controller);
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+  }
+
+  playAnimation() {
+    _controller.reset();
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,53 +39,64 @@ class _HorizontalTabLayoutState extends State<HorizontalTabLayout> {
             left: -20.0,
             bottom: 0,
             top: 0,
+            width: 110,
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 48),
+              padding: EdgeInsets.symmetric(vertical: 80),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   TabText(
                     text: "Media",
                     isSelected: selectedTabIndex == 0,
-                    onTabTap: (){
+                    onTabTap: () {
                       onTabTap(0);
                     },
                   ),
                   TabText(
                     text: "Streamers",
                     isSelected: selectedTabIndex == 1,
-                    onTabTap: (){
+                    onTabTap: () {
                       onTabTap(1);
                     },
                   ),
                   TabText(
                     text: "Forum",
                     isSelected: selectedTabIndex == 2,
-                    onTabTap: (){
+                    onTabTap: () {
                       onTabTap(2);
                     },
                   )
                 ],
               ),
             ),
-          )
+          ),
+          FutureBuilder(
+              future: playAnimation(),
+              builder: (context, snapshot) {
+                return FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: SlideTransition(position: _animation,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: getList(selectedTabIndex),
+                  ),),
+                );
+              })
         ],
       ),
     );
   }
-  onTabTap(int index){
+
+  onTabTap(int index) {
     setState(() {
       selectedTabIndex = index;
     });
   }
+
+  List<Widget> getList(index){
+      return [Container(), Text("adss")];
+  }
 }
-
-
-
-
-
-
-
 
 
 
