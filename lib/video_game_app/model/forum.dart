311ac88a7b0411501bb1d08f3e233b8f6f9bf484
Dreamwalker <aristojeff@gmail.com>
2019-04-26 22:04:34 +0900

    Add Model

--- lib/video_game_app/model/forum.dart
+++ lib/video_game_app/model/forum.dart
@@ -1,5 +1,17 @@
 
 
+import 'package:flutter_notebook/video_game_app/model/topic.dart';
+
 class Forum{
+  final String title;
+  final String imagePath;
+  final String rank;
+  final List<Topic> topics;
+  final String threads;
+  final String subs;
+
+  Forum(this.title, this.imagePath, this.rank, this.topics, this.threads,
+      this.subs);
+
 
 }
--- lib/video_game_app/model/topic.dart
+++ lib/video_game_app/model/topic.dart
@@ -1,1 +1,25 @@
 
+class Topic{
+  final String question;
+  final String recentAnswer;
+  final String answerCount;
+
+  Topic({this.question, this.recentAnswer, this.answerCount});
+}
+
+final fortniteTopics =[
+  Topic(
+    question: "Should we drop early?",
+    recentAnswer: "I hear this question often, so I figured I'd start a thread. When I drop early it usually"
+      ,
+    answerCount: "1523"
+  ),
+  Topic(
+    question: "Quitting FN because of this",
+    recentAnswer:
+    "Literally done with game until they balance the map better so that the blue corner",
+    answerCount: "238",
+  ),
+];
+
+final pubgTopics = fortniteTopics;
