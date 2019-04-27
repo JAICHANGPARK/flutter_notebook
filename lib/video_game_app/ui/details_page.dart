a047ac45eaa6ce8483f11c8321598f1998398c6f
Dreamwalker <aristojeff@gmail.com>
2019-04-27 23:18:39 +0900

    Update details_page.dart

--- lib/video_game_app/ui/details_page.dart
+++ lib/video_game_app/ui/details_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook/video_game_app/model/forum.dart';
+import 'package:flutter_notebook/video_game_app/styles/coclors.dart';
+import 'package:flutter_notebook/video_game_app/styles/text_styles.dart';
 import 'package:flutter_notebook/video_game_app/ui/app_background.dart';
+import 'package:flutter_notebook/video_game_app/ui/forum_ui/forum_detail_widget.dart';
 
 class DetailsPage extends StatefulWidget {
   final Forum forum;
@@ -12,26 +15,29 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage>
-with SingleTickerProviderStateMixin{
+    with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> _fadeAnimation;
   Animation<double> _scaleAnimation;
   Animation<Offset> _offsetAnimation;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = AnimationController(vsync: this,
-    duration: Duration(milliseconds: 500));
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
     _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
     _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
-    _offsetAnimation = Tween<Offset>(begin: Offset(0, 1),
-    end: Offset(0, 0)).animate(_controller);
+    _offsetAnimation = Tween<Offset>(begin: Offset(0, 1), end: Offset(0, 0))
+        .animate(_controller);
   }
-  _playAnimation(){
+
+  _playAnimation() {
     _controller.reset();
     _controller.forward();
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -46,27 +52,101 @@ with SingleTickerProviderStateMixin{
         children: <Widget>[
           AppBackground(
             firstColor: firstOrangeCircleColor,
+            secondColor: secondOrangeCircleColor,
+            thirdColor: thirdOrangeCircleColor,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 60.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              FutureBuilder(
+                future: _playAnimation(),
+                builder: (context, sanpshot) {
+                  return FadeTransition(
+                    opacity: _fadeAnimation,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0, right: 100.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          LabelValueWidget(
+                            value: widget.forum.topics.length.toString(),
+                            label: "topics",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          ),
+                          LabelValueWidget(
+                            value: widget.forum.threads,
+                            label: "threads",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          ),
+                          LabelValueWidget(
+                            value: widget.forum.subs,
+                            label: "subs",
+                            labelStyle: whiteLabelTextStyle,
+                            valueStyle: whiteValueTextStyle,
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Hero(
+                tag: widget.forum.title,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60.0),
+                  ),
+                  child: Image.network(widget.forum.imagePath),
+                ),
+              )
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(60.0)),
+              child: Container(
+                height: 300.0,
+                color: Colors.white,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 32.0,
+                          left: 16.0,
+                          right: 16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Topics",
+                        style: subHeadingStyle.copyWith(fontSize: 26.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           )
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
