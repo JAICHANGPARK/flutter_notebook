12cc8e1a6d04fedbb6287f93c4f6a21d38b5a01b
Dreamwalker <aristojeff@gmail.com>
2019-05-01 20:11:15 +0900

    Update main_page.dart

--- lib/starbucks_app/main_page.dart
+++ lib/starbucks_app/main_page.dart
@@ -113,6 +113,14 @@ class _HomePageState extends State<HomePage> {
               height: MediaQuery.of(context).size.height - 300,
               child: ListView(
                 scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  _buildCoffeeItem(
+                      imgPath:
+                          "https://d3vgbguy0yofad.cloudfront.net/resource/products/images/4524785384323_1_l.jpg",
+                      productName: "シルバー ニードル ハイボール",
+                      productType: "ICED",
+                      price: "¥1,800"),
+                ],
               ),
             ),
           )
@@ -120,26 +128,101 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  _buildItem(String productName, int count){
+
+  _buildCoffeeItem(
+      {String imgPath, String productName, String productType, String price}) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        width: 225.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 4.0,
+                  blurRadius: 4.0)
+            ]),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 200.0,
+                  width: 225.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15.0),
+                          topRight: Radius.circular(15.0)),
+                      image: DecorationImage(
+                          image: NetworkImage(imgPath), fit: BoxFit.cover)),
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    productName,
+                    style: TextStyle(
+                      fontFamily: 'Raleway',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    productType,
+                    style: TextStyle(
+                      fontFamily: 'Raleway',
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15.0,),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: Container(
+                    height: 0.4,
+                    color: Colors.grey.withOpacity(0.4),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _buildItem(String productName, int count) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(4, 10, 15, 10),
-      child: AnimatedContainer(duration: Duration(milliseconds: 500),
-      curve: Curves.easeIn,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: switchHighlight(productName),
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 4.0,
-            spreadRadius: 2.0,
-            color: switchShadow(productName),
-          )
-        ]
-      ),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeIn,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: switchHighlight(productName),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 4.0,
+                spreadRadius: 2.0,
+                color: switchShadow(productName),
+              )
+            ]),
         height: 50.0,
         width: 125.0,
         child: InkWell(
-          onTap: (){
+          onTap: () {
             setState(() {
               selectedItem = productName;
             });
@@ -147,27 +230,31 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: 10.0,),
+              SizedBox(
+                height: 10.0,
+              ),
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
-                child: Text(count.toString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17.0,
-                  fontFamily: 'Raleway',
-                  color: switchHighlightColor(productName)
-                ),),
+                child: Text(
+                  count.toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17.0,
+                      fontFamily: 'Raleway',
+                      color: switchHighlightColor(productName)),
+                ),
               ),
-              SizedBox(height: 7.0,),
+              SizedBox(
+                height: 7.0,
+              ),
               Padding(
                 padding: const EdgeInsets.only(left: 10.0),
                 child: Text(
                   productName,
                   style: TextStyle(
-                    fontFamily: 'Raleway',
-                    fontSize: 15.0,
-                    color: switchHighlightColor(productName)
-                  ),
+                      fontFamily: 'Raleway',
+                      fontSize: 15.0,
+                      color: switchHighlightColor(productName)),
                 ),
               )
             ],
@@ -176,13 +263,15 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  switchHighlight(productName){
-    if(productName == selectedItem){
+
+  switchHighlight(productName) {
+    if (productName == selectedItem) {
       return Color(0xff116d51);
-    }else{
+    } else {
       return Colors.grey.withOpacity(0.3);
     }
   }
+
   switchHighlightColor(prodName) {
     if (prodName == selectedItem) {
       return Colors.white;
@@ -190,10 +279,11 @@ class _HomePageState extends State<HomePage> {
       return Colors.black;
     }
   }
-  switchShadow(productName){
-    if(productName == selectedItem){
+
+  switchShadow(productName) {
+    if (productName == selectedItem) {
       return Color(0xff116d51).withOpacity(0.4);
-    }else{
+    } else {
       return Colors.transparent;
     }
   }
