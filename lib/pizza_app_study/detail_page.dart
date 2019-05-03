458c7b4658dae6941633694e375b38619a97092a
Dreamwalker <aristojeff@gmail.com>
2019-05-03 23:07:54 +0900

    Update detail_page.dart

--- lib/pizza_app_study/detail_page.dart
+++ lib/pizza_app_study/detail_page.dart
@@ -53,8 +53,220 @@ class ForegroundContent extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        PizzaImage(pizzaObject.image),
+        SizedBox(
+          height: 30,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 105, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TitleText(pizzaObject.name),
+              SizedBox(
+                height: 20.0,
+              ),
+              StarRating(pizzaObject.starRating),
+              SizedBox(
+                height: 20.0,
+              ),
+              Description(pizzaObject.desc),
+              SizedBox(
+                height: 20,
+              ),
+              Price(pizzaObject.price),
+              SizedBox(
+                height: 20,
+              ),
+              BottomButtons(),
+              SizedBox(height: 35,)
+            ],
+          ),
         )
       ],
     );
   }
 }
+
+class PizzaImage extends StatelessWidget {
+  final String imageURI;
+
+  const PizzaImage(this.imageURI);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 300,
+      child: Image.asset(imageURI),
+    );
+  }
+}
+class TitleText extends StatelessWidget {
+  final String pizzaName;
+  final double _fontSize = 40;
+
+  TitleText(this.pizzaName);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+            text: pizzaName,
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: _fontSize,
+                fontFamily: 'slabo',
+                fontWeight: FontWeight.w500)),
+        TextSpan(
+            text: " Pizza",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: _fontSize,
+                fontFamily: "slabo",
+                fontWeight: FontWeight.w600))
+      ]),
+    );
+  }
+}
+class StarRating extends StatelessWidget {
+  final double rating;
+
+  StarRating(this.rating);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          rating.toString(),
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Icon(
+          Icons.star,
+          color: Colors.yellow[600],
+          size: 25,
+        )
+      ],
+    );
+  }
+}
+class Description extends StatelessWidget {
+  final String desc;
+
+  Description(this.desc);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      desc,
+      softWrap: true,
+      style: TextStyle(
+          color: Colors.black87,
+          letterSpacing: 1.3,
+          fontSize: 17,
+          textBaseline: TextBaseline.alphabetic),
+    );
+  }
+}
+class Price extends StatelessWidget {
+  final double price;
+  Price(this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "\$$price",
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 25,
+        fontWeight: FontWeight.w700
+      ),
+    );
+  }
+}
+
+class BottomButtons extends StatefulWidget {
+  @override
+  _BottomButtonsState createState() => _BottomButtonsState();
+}
+
+class _BottomButtonsState extends State<BottomButtons> {
+  bool isFav = false;
+  bool isCart = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isFav = false;
+    isCart = false;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Colors.black,
+                width: 5
+              )
+            )
+          ),
+          child: FlatButton(onPressed: (){
+            setState(() {
+              isCart = !isCart;
+            });
+          }, child: Text(
+            isCart ? "Remove from Cart" : "Add to cart",
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 18.0
+            ),
+          )),
+        ),
+        FloatingActionButton(
+          onPressed: (){
+            setState(() {
+              isFav = !isFav;
+            });
+          },
+          child: Icon(
+            isFav ? Icons.favorite : Icons.favorite_border,
+            color: isFav? Colors.red : Colors.black,
+          ),
+          backgroundColor: Colors.white,
+          elevation: 3,
+        )
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
