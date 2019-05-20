b46c43d27b8eb0fe9645ca0d4ec75f68d96ef968
Dreamwalker <aristojeff@gmail.com>
2019-05-20 22:15:09 +0900

    Update detail_page.dart

--- lib/muji_app_clone/detail_page.dart
+++ lib/muji_app_clone/detail_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -39,94 +37,207 @@ class _DetailFoodPageState extends State<DetailFoodPage> {
           InkWell(
             onTap: () {},
             child: Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: IconButton(icon: Icon(Icons.shopping_cart,color: MujiColor,), onPressed: (){})
-            ),
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton(
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      color: MujiColor,
+                    ),
+                    onPressed: () {})),
           )
         ],
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                top: 0,
-                left: 0,
-                right: 0,
-                child: _buildAppBar(),
+      backgroundColor: Colors.grey,
+      body: LayoutBuilder(
+        builder: (context, viewportConstraints){
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                  maxHeight:viewportConstraints.maxHeight
               ),
-              Positioned(
-                top: 70,
-                left: 0,
-                right: 0,
-                child: CarouselSlider(
-                  autoPlay: true,
-                  viewportFraction: 1.0,
-                  items: <Widget>[
-                    Builder(
-                      builder: (context){
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 400,
-                          decoration: BoxDecoration(
-                            color: MujiColor,
-                            image: DecorationImage(image:
-                            NetworkImage("https://img.muji.net/img/item/4550182150841_1260.jpg"),
-                            fit: BoxFit.cover)
-                          ),
-                        );
-                      },
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: _buildAppBar(),
+                  ),
+                  Positioned(
+                    top: 70,
+                    left: 0,
+                    right: 0,
+                    child: CarouselSlider(
+                      autoPlay: true,
+                      viewportFraction: 1.0,
+                      items: <Widget>[
+                        Builder(
+                          builder: (context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 320,
+                              decoration: BoxDecoration(
+                                  color: MujiColor,
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          "https://img.muji.net/img/item/4550182150841_1260.jpg"),
+                                      fit: BoxFit.cover)),
+                            );
+                          },
+                        ),
+                        Builder(
+                          builder: (context) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width,
+                              height: 320,
+                              decoration: BoxDecoration(
+                                  color: MujiColor,
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          "https://img.muji.net/img/item/4550002861308_1260.jpg"),
+                                      fit: BoxFit.cover)),
+                            );
+                          },
+                        )
+                      ],
                     ),
-                    Builder(
-                      builder: (context){
-                        return Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 400,
-                          decoration: BoxDecoration(
-                              color: MujiColor,
-                              image: DecorationImage(image:
-                              NetworkImage("https://img.muji.net/img/item/4550002861308_1260.jpg"),
-                                  fit: BoxFit.cover)
+                  ),
+                  Positioned(
+                    top: 300,
+                    left: 0,
+                    right: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 0),
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                            top: 8, bottom: 8, left: 16, right: 16),
+                        decoration: BoxDecoration(color: Colors.white),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "素材を生かしたカレー　キーマ　１８０ｇ （１人前）",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 1.5),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Container(
+                              height: 24,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(4)),
+                              child: Center(
+                                child: Text(
+                                  "コンビニ受取可",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                                "牛肉の旨みと炒め玉ねぎの甘みを生かし、数種のスパイスで奥行きのある辛さに仕あげました。満足感がある大盛りカレーです。"),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "もっと見る",
+                                style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Divider(
+                              color: Colors.black,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text("税込"),
+                                Text(
+                                  "350円 <",
+                                  style: TextStyle(color: MujiColor, fontSize: 24),
+                                ),
+                                Expanded(
+                                  child: Container(),
+                                ),
+                                Text(
+                                  "配送料無料",
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                            Text("商品番号82505672"),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 80,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.white,
+                      child: Column(
+                        children: <Widget>[
+                          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("数量"),
+                              Text("1"),
+                              Text("あり")
+                            ],
                           ),
-                        );
-                      },
-                    )
-                  ],
-                ),
-              )
+                          Divider(color: Colors.black,),
+                          Row(
 
-            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                height: 30
+                                width: 300,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(30),
+                                  color: MujiColor
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: (){},
+                                icon: Icon(Icons.favorite,color: Colors.grey,),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
+        },
+
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
