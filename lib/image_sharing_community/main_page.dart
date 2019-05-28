079009848a1d2babc40f968e3186ace5fd1cd498
Dreamwalker <aristojeff@gmail.com>
2019-05-28 22:26:54 +0900

    Update main_page.dart

--- lib/image_sharing_community/main_page.dart
+++ lib/image_sharing_community/main_page.dart
@@ -18,7 +18,79 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+        title: Text("Dreamwalker", style: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+        ),),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.arrow_back_ios),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 4,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 8,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.green),
+          ),
+
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.orange),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 8,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.redAccent),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(color: Colors.cyan),
+          ),
+
+        ],
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
