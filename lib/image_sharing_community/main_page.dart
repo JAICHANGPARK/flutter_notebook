1cb4e1175925902d041bcde72a616c72811f68dc
Dreamwalker <aristojeff@gmail.com>
2019-05-28 23:02:31 +0900

    Update main_page.dart

--- lib/image_sharing_community/main_page.dart
+++ lib/image_sharing_community/main_page.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 
 class ImageShareCommunity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
@@ -20,14 +20,12 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(
-          color: Colors.black
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Dreamwalker",
+          style: TextStyle(
+              fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
         ),
-        title: Text("Dreamwalker", style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.black
-        ),),
         centerTitle: true,
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -35,7 +33,7 @@ class _MainPageState extends State<MainPage> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.more_horiz),
-            onPressed: (){},
+            onPressed: () {},
           )
         ],
       ),
@@ -47,50 +45,296 @@ class _MainPageState extends State<MainPage> {
             height: MediaQuery.of(context).size.height / 4,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-              color: Colors.blue,
+//              color: Colors.blue,
+                ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  width: 80,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
+                          fit: BoxFit.cover)),
+                ),
+                Text(
+                  "Dreamwalker",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "Always believe in yourself",
+                  style: TextStyle(color: Colors.grey.withOpacity(0.4)),
+                )
+              ],
             ),
           ),
           Container(
             height: MediaQuery.of(context).size.height / 8,
             width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(color: Colors.green),
+            decoration: BoxDecoration(
+//              color: Colors.green,
+                ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "11",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text("Classify")
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "85",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text("Classify")
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "37",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text("Classify")
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(color: Colors.orange),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+//                color: Colors.orange,
+                  ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(17),
+                          border: Border(
+                            right: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            bottom: BorderSide(color: Colors.black),
+                          )),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(17),
+                                  border: Border(
+                                    right: BorderSide(color: Colors.black),
+                                    left: BorderSide(color: Colors.black),
+                                    top: BorderSide(color: Colors.black),
+                                    bottom: BorderSide(color: Colors.black),
+                                  )),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17),
+                                border: Border(
+                                  right: BorderSide(color: Colors.black),
+                                  left: BorderSide(color: Colors.black),
+                                  top: BorderSide(color: Colors.black),
+                                  bottom: BorderSide(color: Colors.black),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "All 99+",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
+            padding: EdgeInsets.all(16),
             height: MediaQuery.of(context).size.height / 8,
             width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(color: Colors.redAccent),
+            decoration: BoxDecoration(
+//              color: Colors.redAccent,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Scenery photo", style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    Text("48 minutes ago",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey
+                      ),)
+                  ],
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Center(
+                    child: Icon(Icons.more_horiz),
+                  ),
+                )
+              ],
+            ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(color: Colors.cyan),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: MediaQuery.of(context).size.height / 2.5,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+//                color: Colors.orange,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.all(8),
+                      height: double.infinity,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(17),
+                          border: Border(
+                            right: BorderSide(color: Colors.black),
+                            left: BorderSide(color: Colors.black),
+                            top: BorderSide(color: Colors.black),
+                            bottom: BorderSide(color: Colors.black),
+                          )),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      child: Column(
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(17),
+                                  border: Border(
+                                    right: BorderSide(color: Colors.black),
+                                    left: BorderSide(color: Colors.black),
+                                    top: BorderSide(color: Colors.black),
+                                    bottom: BorderSide(color: Colors.black),
+                                  )),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(17),
+                                border: Border(
+                                  right: BorderSide(color: Colors.black),
+                                  left: BorderSide(color: Colors.black),
+                                  top: BorderSide(color: Colors.black),
+                                  bottom: BorderSide(color: Colors.black),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "All 99+",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-
         ],
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
