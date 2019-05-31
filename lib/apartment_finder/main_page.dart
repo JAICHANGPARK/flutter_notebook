3f4fcb3eb21ed1a70f3eefcc23fe7f1657a53d2d
Dreamwalker <aristojeff@gmail.com>
2019-06-01 00:05:50 +0900

    Update main_page.dart

--- lib/apartment_finder/main_page.dart
+++ lib/apartment_finder/main_page.dart
@@ -43,25 +43,62 @@ class _MainPageState extends State<MainPage> {
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: <Widget>[
                           IconButton(
-                            icon: Icon(Icons.arrow_back, color: Colors.white,
-                            size: 32,),
-                            onPressed: (){},
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                              size: 32,
+                            ),
+                            onPressed: () {},
                           ),
                           IconButton(
-                            icon: Icon(Icons.more_vert, color: Colors.white,
-                                size: 32),
-                            onPressed: (){},
+                            icon: Icon(Icons.more_vert,
+                                color: Colors.white, size: 32),
+                            onPressed: () {},
                           ),
                         ],
                       ),
                     ),
                     Positioned(
-                      bottom: 70,
+                      bottom: 90,
                       left: 0,
                       right: 0,
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
-                          
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey.withOpacity(0.6)),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white),
+                          )
                         ],
                       ),
                     )
@@ -74,12 +111,54 @@ class _MainPageState extends State<MainPage> {
               right: 0,
               top: MediaQuery.of(context).size.height / 2,
               child: Container(
+                padding: const EdgeInsets.only(left: 32, right: 16, bottom: 24),
                 height: MediaQuery.of(context).size.height / 4,
                 decoration: BoxDecoration(
-                  color: Colors.orange,
+                  color: Colors.purple,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(38),
                   ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "\$1,700",
+                          style: TextStyle(
+                            fontSize: 42,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "\$642 William St.",
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.share,
+                            color: Colors.white,),
+                          onPressed: (){},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.favorite_border,
+                            color: Colors.white,),
+                          onPressed: (){},
+                        )
+                      ],
+                    )
+                  ],
                 ),
               ),
             ),
