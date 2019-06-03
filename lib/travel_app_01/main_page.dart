5def92aaab67f5325211ba8f839d102d8aeb65e1
Dreamwalker <aristojeff@gmail.com>
2019-06-03 22:54:33 +0900

    Add travel app

---
+++ lib/travel_app_01/main_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+class TravelApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
