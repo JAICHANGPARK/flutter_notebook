97f2949d63024ede01529e0ea0b07663afb03b62
Dreamwalker <aristojeff@gmail.com>
2019-06-06 00:00:16 +0900

    Update main_page.dart

--- lib/credit_card_banking_app/main_page.dart
+++ lib/credit_card_banking_app/main_page.dart
@@ -21,20 +21,62 @@ class _HomePageState extends State<HomePage> {
       body: Scaffold(
         body: Column(
           children: <Widget>[
+            Spacer(),
             Expanded(
-              flex: 1,
+              flex: 2,
               child: Container(
-                color: Colors.blueGrey,
+//                color: Colors.blueGrey,
+              margin: const EdgeInsets.only(left: 16, right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(17),
+                  border: Border.all(color: Colors.grey)
+                ),  
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: const EdgeInsets.only(left: 36),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Your Cards", style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2
+                  ),),
+                ),
               ),
             ),
             Expanded(
               flex: 6,
               child: Container(
-                color: Colors.red
+                padding: const EdgeInsets.only(left: 36),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      decoration: BoxDecoration(
+//                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(color: Colors.black)
+                      ),
+                    ),
+                    SizedBox(width: 16,),
+                    Container(
+                      width: 180,
+                      margin: const EdgeInsets.only(bottom: 36),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.black)
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Expanded(
-              flex: 5,
+              flex: 4,
               child: Container(
                   color: Colors.blue
               ),
