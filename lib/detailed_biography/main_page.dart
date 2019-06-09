b35322463e1c72101a5fadcc97e41726f61233ce
Dreamwalker <aristojeff@gmail.com>
2019-06-09 22:31:21 +0900

    update

--- lib/detailed_biography/main_page.dart
+++ lib/detailed_biography/main_page.dart
@@ -28,7 +28,7 @@ class _ListPageState extends State<ListPage> {
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
-                color: Colors.white,
+
               ),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -63,16 +63,87 @@ class _ListPageState extends State<ListPage> {
             flex: 8,
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.blue,
+//                color: Colors.blue,
               ),
               child: ListView.builder(
-                scrollDirection: Axis.vertical,
+                  scrollDirection: Axis.vertical,
                   shrinkWrap: true,
                   itemCount: mainUserList.length,
                   itemBuilder: (context, index) {
                     return Container(
-                      margin: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(color: Colors.orange),
+                      height: 74,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.only(
+                          top: 8, bottom: 8, left: 16, right: 16),
+                      decoration: BoxDecoration(),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              child: Center(
+                                child: Container(
+                                  height: 60,
+                                  width: 60,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.redAccent,
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              mainUserList[index]
+                                                  .userImagePath))),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Container(
+                              decoration: BoxDecoration(
+//                                  color: Colors.green
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Text(
+                                    mainUserList[index].userName,
+                                    style: TextStyle(
+                                        color: Colors.red,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                        fontFamily: 'Montserrat'),
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Text(mainUserList[index].userDetail,
+                                      style: TextStyle(
+                                          color: Colors.red.withOpacity(0.6),
+                                          fontSize: 12,
+                                          fontFamily: 'Montserrat')),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              decoration: BoxDecoration(),
+                              child: Center(
+                                child:
+                                mainUserList[index].userBookmark ?
+                                IconButton(
+                                    icon: Icon(Icons.bookmark, color: Colors.red,),
+                                    onPressed: (){}):
+                                IconButton(
+                                    icon: Icon(Icons.bookmark_border, color: Colors.red),
+                                    onPressed: (){}),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     );
                   }),
             ),
