f5ee04c9348475e17561eed81d4c9b8f46d06b81
Dreamwalker <aristojeff@gmail.com>
2019-06-19 22:58:19 +0900

    Update main_page.dart

--- lib/app_freelancers/main_page.dart
+++ lib/app_freelancers/main_page.dart
@@ -18,13 +18,21 @@ class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> _animation;
-
+  Animation<double> _animation2;
   @override
   void initState() {
-    _controller = AnimationController(vsync: this);
-    _animation = Tween<double>(begin: 300, end: 150).animate(_controller);
+
     super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _animation = Tween<double>(begin: 300, end: 72).animate(_controller)..addListener((){
+      setState(() {
+
+      });
+    });
+
   }
+
 
   @override
   void dispose() {
@@ -34,68 +42,141 @@ class _MainPageState extends State<MainPage>
 
   @override
   Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height / 1.8;
     return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.play_arrow),
-              onPressed: () {
-                _controller.forward();
-              },
-            )
-          ],
-        ),
+      backgroundColor: Colors.blueGrey,
         body: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 24,
-              left: 0,
-              child: IconButton(
-                  icon: Icon(
-                    Icons.arrow_back,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {}),
-            ),
-            Positioned(
-              top: 40,
-              left: 0,
-              child: Row(
+      children: <Widget>[
+        Positioned(
+          top: 100,
+          left: 16,
+          right: 16,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Text("Client's name"),
-                      Text("3 current project of 11 in total"),
-                    ],
-                  ),
-
-                  Icon(Icons.more_vert,size: 42, color: Colors.white,)
+                  Text("Client's name",
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24
+                  ),),
+                  Text("3 current project of 11 in total",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14
+                    ),),
                 ],
               ),
+              Icon(
+                Icons.more_vert,
+                size: 42,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            height: MediaQuery.of(context).size.height / 1.3,
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(60), topLeft: Radius.circular(60)),
             ),
-            AnimatedContainer(
-              duration: Duration(seconds: 1),
-              height: _animation.value,
-              decoration: BoxDecoration(color: Colors.red),
-            )
-          ],
-        ));
+            child: Column(
+              children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    _controller.forward();
+                    setState(() {
+                      _height = 150;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Project",
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.add),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: Container(
+            height: _animation.value,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(60),
+                    topLeft: Radius.circular(60))),
+            child: Column(
+              children: <Widget>[
+                InkWell(
+                  onTap: (){
+                    _controller.reverse();
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text("Invoices"),
+                        CircleAvatar(
+                          backgroundColor: Colors.blue,
+                          child: Icon(Icons.add),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+//          child: AnimatedContainer(
+//            height: 150,
+//            curve: Curves.easeIn,
+//            decoration: BoxDecoration(
+//                color: Colors.red,
+//                borderRadius: BorderRadius.only(
+//                    topRight: Radius.circular(60),
+//                    topLeft: Radius.circular(60))),
+//            duration: Duration(seconds: 1),
+//          ),
+        ),
+        Positioned(
+          top: 24,
+          left: 0,
+          child: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  _controller.forward();
+                });
+              }),
+        ),
+      ],
+    ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
