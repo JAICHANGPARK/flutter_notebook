29b7bff538fcb697afca1121a97c0685f1a34e80
Dreamwalker <aristojeff@gmail.com>
2019-06-24 22:54:06 +0900

    Create home_page.dart

---
+++ lib/job_listing/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class JobListing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black45,
+      body: Column(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+
