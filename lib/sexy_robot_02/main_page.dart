eb1dbeb0bfc026e9d1fbad111cf33144a5a5a29d
Dreamwalker <aristojeff@gmail.com>
2019-06-27 21:43:33 +0900

    Update main_page.dart

--- lib/sexy_robot_02/main_page.dart
+++ lib/sexy_robot_02/main_page.dart
@@ -105,27 +105,117 @@ class _MainPageState extends State<MainPage> {
               ),
             ),
           ),
+          SizedBox(height: 24,),
           //TODO Implement mechanical center
           Expanded(
             flex: 7,
             child: Container(
-              decoration: BoxDecoration(border: Border.all()),
               child: Stack(
                 children: <Widget>[
+
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    bottom: 0,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(right: 16),
+                            width: MediaQuery.of(context).size.width / 2.3,
+                            decoration: BoxDecoration(
+                              color: sexyColor,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                Expanded(
+                                  flex: 8,
+                                  child: Transform.translate(
+                                    offset: Offset(-64, 16),
+//                                    child: Container(
+//                                      decoration: BoxDecoration(
+//                                        image: DecorationImage(
+//                                            image: NetworkImage(
+//                                                "https://i.pinimg.com/originals/e5/c2/27/e5c2276a61605f6c3def9d617f5b5498.png")),
+//                                      ),
+//                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                      padding: const EdgeInsets.all(12),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Row(
+                                            children: <Widget>[
+                                              Text(
+                                                ""
+                                                "Silver Wing",
+                                                style: TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                              SizedBox(
+                                                width: 8,
+                                              ),
+                                              Container(
+                                                height: 8,
+                                                width: 8,
+                                                decoration: BoxDecoration(
+                                                    color: Colors.white,
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                        color: Colors.grey,
+                                                        width: 2)),
+                                              )
+                                            ],
+                                          ),
+                                          Text(
+                                            "White",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ],
+                                      )),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 16),
+                            width: MediaQuery.of(context).size.width / 2.5,
+                            color: Colors.yellow,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 140,
+                    top: 24,
+                    child: Image.network("https://i.pinimg.com/originals/e5/c2/27/e5c2276a61605f6c3def9d617f5b5498.png",
+                      width: 180,),
+                  ),
                   Positioned(
                     left: 16,
                     top: 16,
                     child: Container(
                       height: 60,
                       width: MediaQuery.of(context).size.width / 2.5,
-                      child: Text("Mechanical\nCenter",style:
-                        TextStyle(
-                          color: sexyColor,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24
-                        ),),
+                      child: Text(
+                        "Mechanical\nCenter",
+                        style: TextStyle(
+                            color: sexyColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24),
+                      ),
                     ),
-
                   ),
                   Positioned(
                     bottom: 16,
@@ -141,169 +231,175 @@ class _MainPageState extends State<MainPage> {
                             flex: 1,
                             child: Align(
                               alignment: Alignment.bottomLeft,
-                              child: Text("Available Size",
-                              style: TextStyle(
-                                color: sexyColor.withOpacity(0.5),
-                              ),),
+                              child: Text(
+                                "Available Size",
+                                style: TextStyle(
+                                  color: sexyColor.withOpacity(0.5),
+                                ),
+                              ),
                             ),
                           ),
-                          SizedBox(height: 16,),
+                          SizedBox(
+                            height: 16,
+                          ),
                           Expanded(
                             flex: 1,
                             child: Row(
                               children: <Widget>[
-                                Expanded(flex: 1,
-                                child: Container(
-                                  margin: const EdgeInsets.only(right: 8),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    border: Border.all(
-                                      color: sexyColor.withOpacity(0.5),
-                                      
-                                    ),
-                                    
-                                  ),
-                                  child: Center(
-                                    child: Text("XS",style:
-                                      TextStyle(
-                                        color: sexyColor
-                                      ),),
-                                  ),
-                                ),),
-                                Expanded(flex: 1,
+                                Expanded(
+                                  flex: 1,
                                   child: Container(
                                     margin: const EdgeInsets.only(right: 8),
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(8),
                                       border: Border.all(
                                         color: sexyColor.withOpacity(0.5),
-
                                       ),
-
                                     ),
                                     child: Center(
-                                      child: Text("S",style:
-                                      TextStyle(
-                                          color: sexyColor
-                                      ),),
+                                      child: Text(
+                                        "XS",
+                                        style: TextStyle(color: sexyColor),
+                                      ),
                                     ),
-                                  ),),
-                                Expanded(flex: 1,
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
                                   child: Container(
                                     margin: const EdgeInsets.only(right: 8),
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(8),
                                       border: Border.all(
                                         color: sexyColor.withOpacity(0.5),
-
                                       ),
-
                                     ),
                                     child: Center(
-                                      child: Text("M",style:
-                                      TextStyle(
-                                          color: sexyColor
-                                      ),),
+                                      child: Text(
+                                        "S",
+                                        style: TextStyle(color: sexyColor),
+                                      ),
                                     ),
-                                  ),)
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    margin: const EdgeInsets.only(right: 8),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(
+                                        color: sexyColor.withOpacity(0.5),
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "M",
+                                        style: TextStyle(color: sexyColor),
+                                      ),
+                                    ),
+                                  ),
+                                )
                               ],
                             ),
                           )
-
                         ],
                       ),
                     ),
                   ),
-                  Positioned(
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width/ 2,
-
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-
-                        children: <Widget>[
-                          Container(
-                            margin: const EdgeInsets.only(right: 16),
-                            width: MediaQuery.of(context).size.width/ 2.3,
-                            decoration: BoxDecoration(
-                              color: sexyColor,
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            child: Column(
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 8,
-                                  child: Container(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Container(
-                                    padding: const EdgeInsets.all(12),
-                                    color: Colors.blueGrey,
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Row(
-                                          children: <Widget>[
-                                            Text(""
-                                                "Silver Wing",
-                                            style: TextStyle(
-                                              color: Colors.white
-                                            ),),
-                                            SizedBox(
-                                              width: 8,
-                                            ),
-                                            Container(
-                                              height: 8,
-                                              width: 8,
-                                              decoration: BoxDecoration(
-                                                color: Colors.white,
-                                                shape: BoxShape.circle,
-                                                border: Border.all(
-                                                  color: Colors.grey,
-                                                  width: 2
-                                                )
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                        Text("White",
-                                          style: TextStyle(
-                                              color: Colors.white
-                                          ),),
-                                      ],
-                                    )
-                                  ),
-
-                                )
-                              ],
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(right: 16),
-                            width: MediaQuery.of(context).size.width/ 2.5,
-                            color: Colors.yellow,
-                          )
-                        ],
-                      ),
-                    ),
-                  )
                 ],
               ),
             ),
-
           ),
           //TODO Implement spec
+          SizedBox(height: 8,),
           Expanded(
             flex: 4,
             child: Container(
-              decoration: BoxDecoration(border: Border.all()),
+              padding: const EdgeInsets.only(top: 16, left: 16),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("ENDURANCE",
+                        style: TextStyle(
+                          color: sexyColor,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),),
+                        Text("ERGONOMICS",
+                          style: TextStyle(
+                            color: sexyColor,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),),
+                        Text("NOISE",
+                          style: TextStyle(
+                            color: sexyColor,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Padding(
+                          padding:EdgeInsets.only(right: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text("60 YEARS",
+                                style: TextStyle(
+                                    color: sexyColor.withOpacity(0.5)
+                                ),),
+                              Icon(Icons.arrow_forward_ios, size: 16,
+                                  color: sexyColor.withOpacity(0.5))
+                            ],
+                          ),
+                        ),
+                        Divider(color: sexyColor,),
+                        Padding(
+                          padding:EdgeInsets.only(right: 16),
+
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text("PATENT PROTECTION",
+                                style: TextStyle(
+                                    color: sexyColor.withOpacity(0.5)
+                                ),),
+                              Icon(Icons.arrow_forward_ios, size: 16,
+                                  color: sexyColor.withOpacity(0.5))
+                            ],
+                          ),
+                        ),
+                        Divider(color: sexyColor,),
+                        Padding(
+                          padding:EdgeInsets.only(right: 16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Text("SILENT",style: TextStyle(
+                                color: sexyColor.withOpacity(0.5)
+                              ),),
+                              Icon(Icons.arrow_forward_ios, size: 16,
+                                color: sexyColor.withOpacity(0.5) ,)
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
           //TODO Implement price ui
