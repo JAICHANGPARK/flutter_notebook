6aa594bda982bf72c7d32160ff83f360227614e0
Dreamwalker <aristojeff@gmail.com>
2019-06-28 23:23:35 +0900

    Create app_for_collectors.dart

---
+++ lib/app_for_collectors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AppForCollector extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
