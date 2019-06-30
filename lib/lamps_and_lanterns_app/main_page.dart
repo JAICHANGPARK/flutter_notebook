67ea240b2df92c303c87575e91e8123e583546fe
Dreamwalker <aristojeff@gmail.com>
2019-06-30 18:35:36 +0900

    Create main_page.dart

---
+++ lib/lamps_and_lanterns_app/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LampsAndLanternsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+
