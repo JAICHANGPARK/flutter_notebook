e5daedd43ed6cdc991f4c71beada2d82919e8460
Dreamwalker <aristojeff@gmail.com>
2019-06-30 18:40:59 +0900

    Update main_page.dart

--- lib/lamps_and_lanterns_app/main_page.dart
+++ lib/lamps_and_lanterns_app/main_page.dart
@@ -4,6 +4,7 @@ class LampsAndLanternsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
@@ -16,8 +17,71 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var appColor = Color(0xff387272);
+    return Scaffold(
+      backgroundColor: appColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 1.3,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(64),
+                ),
+              ),
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                      height: 60,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: appColor
+                      ),
+                      child: Center(
+                        child: Icon(Icons.shopping_cart,
+                        color: Colors.white,),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
