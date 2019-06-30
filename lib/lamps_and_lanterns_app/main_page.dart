d15db62d844f2966abc06307dae9b9ca724e2b5f
Dreamwalker <aristojeff@gmail.com>
2019-06-30 18:53:25 +0900

    Update main_page.dart

--- lib/lamps_and_lanterns_app/main_page.dart
+++ lib/lamps_and_lanterns_app/main_page.dart
@@ -9,6 +9,7 @@ class LampsAndLanternsApp extends StatelessWidget {
     );
   }
 }
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -41,12 +42,143 @@ class _MainPageState extends State<MainPage> {
                       height: 60,
                       width: 120,
                       decoration: BoxDecoration(
-                        color: appColor
-                      ),
+                          color: appColor,
+                          borderRadius:
+                              BorderRadius.only(topLeft: Radius.circular(38))),
                       child: Center(
-                        child: Icon(Icons.shopping_cart,
-                        color: Colors.white,),
+                        child: Icon(
+                          Icons.shopping_cart,
+                          size: 34,
+                          color: Colors.white,
+                        ),
                       ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    child: Container(
+                      height: 84,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          color: appColor,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(38))),
+                      child: Center(
+                        child: Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: 38,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 84,
+                    left: 32,
+                    bottom: 32,
+                    child: Container(
+                      width: 200,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Top Picks",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 22,
+                                fontFamily: 'Montserrat'),
+                          ),
+                          Text(
+                            "In china, hangzhou",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                                fontFamily: 'Montserrat'),
+                          ),
+                          SizedBox(
+                            height: 24,
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: <Widget>[
+                              Text(
+                                "\$",
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    color: appColor,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                              Text(
+                                "25",
+                                style: TextStyle(
+                                    color: appColor,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              )
+                            ],
+                          ),
+                          Spacer(),
+                          Text(
+                            "NIGHT LAMP",
+                            style: TextStyle(
+                                color: Colors.grey, fontFamily: 'Montserrat'),
+                          ),
+                          SizedBox(height: 4,),
+                          Text(
+                            "Olivia",
+                            style: TextStyle(
+                                fontSize: 24, fontFamily: 'Montserrat'),
+                          ),
+                          SizedBox(height: 4,),
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                height: 38,
+                                width: 38,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.lightbulb_outline,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Container(
+                                height: 38,
+                                width: 38,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.grey),
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.alarm,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 32,
+                    top: 140,
+                    bottom: 120,
+                    child: Container(
+                      width: 240,
+                      color: Colors.greenAccent,
                     ),
                   )
                 ],
@@ -58,15 +190,6 @@ class _MainPageState extends State<MainPage> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 
