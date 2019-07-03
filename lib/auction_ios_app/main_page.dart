f04f012289e5155fb88c762c3a80bd1ba8df4860
Dreamwalker <aristojeff@gmail.com>
2019-07-03 21:41:40 +0900

    Create main_page.dart

---
+++ lib/auction_ios_app/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class AuctionKawsApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
