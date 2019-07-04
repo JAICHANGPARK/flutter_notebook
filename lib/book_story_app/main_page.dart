1baf97e2fe287edfed3dab2d92cb2b5fabeda3a0
Dreamwalker <aristojeff@gmail.com>
2019-07-04 23:31:31 +0900

    Update main_page.dart

--- lib/book_story_app/main_page.dart
+++ lib/book_story_app/main_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:parallax_image/parallax_image.dart';
 
@@ -11,6 +14,29 @@ class BookStoryApp extends StatelessWidget {
   }
 }
 
+class RevealProgressPainter extends CustomPainter {
+  double _fraction = 0.0;
+  Size _screenSize;
+
+  RevealProgressPainter(this._fraction, this._screenSize);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = Colors.pink
+      ..style = PaintingStyle.fill;
+    final finalRadius =
+        sqrt(pow(_screenSize.width / 2, 2) + pow(_screenSize.height / 2, 2));
+    var radius = 24.0 + finalRadius + _fraction;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(RevealProgressPainter oldDelegate) {
+    return oldDelegate._fraction != _fraction;
+  }
+}
+
 class MainPage extends StatefulWidget {
   @override
   _MainPageState createState() => _MainPageState();
@@ -19,11 +45,29 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  Animation<double> _animation;
+  double _fraction = 0.0;
 
   @override
   void initState() {
-    _controller = AnimationController(vsync: this);
     super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+    Timer(Duration(milliseconds: 3000), () {});
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {
+          _fraction = _animation.value;
+        });
+      });
+  }
+
+  void reveal() {
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.forward();
   }
 
   @override
@@ -50,18 +94,29 @@ class _MainPageState extends State<MainPage>
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 CircleAvatar(
-                  backgroundImage: NetworkImage("https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
+                  backgroundImage: NetworkImage(
+                      "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
                 ),
                 Spacer(),
-                Text("Story Books", style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold
-                ),),
+                Text(
+                  "Story Books",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
                 Spacer(),
-                Icon(Icons.search, color: Colors.white,),
-                SizedBox(width: 16,),
-                Icon(Icons.list, color: Colors.white,)
+                Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Icon(
+                  Icons.list,
+                  color: Colors.white,
+                )
               ],
             ),
           ),
@@ -74,13 +129,16 @@ class _MainPageState extends State<MainPage>
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("The Magic",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
-                ),),
-                SizedBox(height: 4,),
+                Text(
+                  "The Magic",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
                 Container(
                   height: 2,
                   width: 16,
@@ -92,22 +150,48 @@ class _MainPageState extends State<MainPage>
           Container(
             height: MediaQuery.of(context).size.height / 1.5,
             child: ListView(
+              physics: BouncingScrollPhysics(),
               scrollDirection: Axis.horizontal,
               children: <Widget>[
+                InkWell(
+                  onTap: () {
+                    _controller.forward();
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(7),
+                    width: MediaQuery.of(context).size.width - 16,
+                    child: ParallaxImage(
+                      image: NetworkImage(
+                        "https://t3.ftcdn.net/jpg/01/21/39/24/240_F_121392436_TyJ0RrKUxTni7ADl2tEmxhiWB3DQpa99.jpg",
+                      ),
+                      extent: 100.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
                 Container(
                   margin: const EdgeInsets.all(7),
                   width: MediaQuery.of(context).size.width - 16,
                   child: ParallaxImage(
-                    image: NetworkImage("https://t3.ftcdn.net/jpg/01/21/39/24/240_F_121392436_TyJ0RrKUxTni7ADl2tEmxhiWB3DQpa99.jpg"),
-                    extent: 100.0,
-
+                    image: NetworkImage(
+                        "https://voxillustration.com/category/wp-content/uploads/2018/10/Collage-Illustration-1.jpg"),
+                    extent: 80.0,
                   ),
                 ),
-                SizedBox(width: 16,),
+                SizedBox(
+                  width: 16,
+                ),
                 Container(
                   margin: const EdgeInsets.all(7),
                   width: MediaQuery.of(context).size.width - 16,
-                  color: Colors.redAccent,
+                  child: ParallaxImage(
+                    image: NetworkImage(
+                      "https://t3.ftcdn.net/jpg/01/21/39/24/240_F_121392436_TyJ0RrKUxTni7ADl2tEmxhiWB3DQpa99.jpg",
+                    ),
+                    extent: 100.0,
+                  ),
                 ),
               ],
             ),
@@ -160,20 +244,3 @@ class _MainPageState extends State<MainPage>
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
