e7ed250520ac8da2a1c72307cb0ea29301145a0e
Dreamwalker <aristojeff@gmail.com>
2019-07-06 18:22:05 +0900

    Create main_page.dart

---
+++ lib/user_profile_05/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UserProfile05 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserPage(),
+    );
+  }
+}
+class UserPage extends StatefulWidget {
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
