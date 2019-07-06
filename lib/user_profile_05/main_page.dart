8995ff139ca7ad1a7bb35e6f455c067bf031c444
Dreamwalker <aristojeff@gmail.com>
2019-07-06 18:38:42 +0900

    Update main_page.dart

--- lib/user_profile_05/main_page.dart
+++ lib/user_profile_05/main_page.dart
@@ -9,6 +9,7 @@ class UserProfile05 extends StatelessWidget {
     );
   }
 }
+
 class UserPage extends StatefulWidget {
   @override
   _UserPageState createState() => _UserPageState();
@@ -17,7 +18,118 @@ class UserPage extends StatefulWidget {
 class _UserPageState extends State<UserPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  child: Transform.translate(
+                    offset: Offset(0, -820),
+                    child: Transform.scale(
+                      scale: 1.5,
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 2,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            gradient: LinearGradient(
+                                colors: [
+                                  Colors.green,
+                                  Colors.lightGreen,
+                                  Colors.green.withOpacity(1),
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                stops: [0, 0.8, 1],
+                                tileMode: TileMode.clamp)),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 24,
+                  left: 24,
+                  right: 24,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 116,
+                  child: Container(
+                    height: 160,
+                    width: 160,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.redAccent,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars2.githubusercontent.com/u/19484515?s=460&v=4"),
+                            fit: BoxFit.cover)),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Dreamwalker",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.location_on),
+                    Text('Tokyo, Japan'),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Placeholder(),
+          ),
+          Expanded(
+            flex: 5,
+            child: Placeholder(),
+          ),
+        ],
+      ),
+    );
   }
 }
-
